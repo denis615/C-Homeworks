75ec52efc8795323392c7e8bcfd51cc0a0644dcc
denis615 <58919732+denis615@users.noreply.github.com>
2020-03-25 01:19:45 +0100

    Homework Class4

---
+++ Homework Class4/Task1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+       
+            Console.Clear();
+            //Declaring the String as you Described
+            string subString = "Hello from SEDC Codecademy v7.0";
+            Console.WriteLine(subString);
+            //Requesting an input from the user
+            Console.WriteLine("Please enter a number?");
+            string input = Console.ReadLine();
+            //Converting it to an Integrer
+            int n = int.Parse(input);
+            //Making it to Char so we can split it
+            char[] chars = subString.ToCharArray();
+            //Getting the length of the chars so that we can use it in our if statement
+            int charsnumber = chars.Length;
+            //Counter for our while loop
+            int counter1 = 0;
+
+
+            string output1="";
+
+            
+            //Condition to check if the input is less than the length of the char array
+            if (charsnumber>=n) {
+                //Making a While loop 
+                while (counter1 <= n) {
+
+                    
+                    //Making the Charact back to string and adding them to the output
+                     output1 += chars[counter1].ToString();
+                    counter1++;
+
+
+
+                  
+
+                  
+
+
+
+                }
+                //Output
+                Console.WriteLine(output1);
+
+                //Geting the length
+                int outputlenght = output1.Length;
+                Console.WriteLine($"The length of the new string is:{outputlenght}");
+                Console.ReadLine();
+
+
+            }
+            else
+            {// If there is a bigger number or invalid character :D
+                Console.WriteLine("The number you entered is more than the length of the String or is an invalid character. Goodbye");
+                Environment.Exit(0);
+            }
+           
+        }
+    }
+}
---
+++ Homework Class4/Task2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {startAgain:
+            Console.Clear();
+            Console.WriteLine("Please Enter a string of any length");
+            string stringInput = Console.ReadLine();
+            Console.WriteLine("Please enter a character");
+            string characterInput = Console.ReadLine();
+            char[] characterInputToChar = characterInput.ToCharArray();
+            char[] charArray = stringInput.ToCharArray();
+
+
+            int result = 0;
+
+            for (int i = 0; i < stringInput.Length; i++)
+            {
+                if (charArray[i] == characterInputToChar[0])
+                {
+                    result++;
+                }
+
+
+            }
+
+            Console.WriteLine($"That Character is {result} times in your array");
+
+
+            Console.WriteLine("Would you like to Play again? Press Y or N");
+            string answer = Console.ReadLine();
+            string toLower = answer.ToLower();
+            if (toLower == "y")
+            {
+                goto startAgain;
+            }
+            else
+            {
+                Console.WriteLine("Goodbye");
+                Environment.Exit(0);
+            }
+            
+        }
+    }
+}
